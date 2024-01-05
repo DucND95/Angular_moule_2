04c61e15f736d5bc98255cc6e23cfcf7b697635a
NGUYEN DANG DUC <ducnd@aht.local>
2024-01-05 16:55:40 +0700

    push code

---
+++ src/app/_models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    id?: string;
+    username?: string;
+    password?: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    phoneNumber?: string;
+    address?: string;
+    role?: string;
+    token?: string;
+}
